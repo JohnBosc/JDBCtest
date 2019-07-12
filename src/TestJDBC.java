89d159c05ecb485afd2f73ff4923968c3d2d7d62
JohnBosc <jnrmengue@gmail.com>
2019-07-12 22:34:31 +0100

    JDBC connection test - FUN.fr

---
+++ src/TestJDBC.java
@@ -0,0 +1,23 @@
+import java.sql.*;
+
+public class TestJDBC {
+    public static void main (String [] args) throws Exception {
+        // Charger la class du driver
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection cx = DriverManager.getConnection("jdbc:mysql://localhost:3309/cef506?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "");
+            Statement st = cx.createStatement();
+            ResultSet rs = st.executeQuery("SELECT * FROM destination");
+            while (rs.next()) {
+                String name = rs.getString("name");
+                int id = rs.getInt("id");
+                System.out.println("ID " + id + " is for " + name);
+            }
+            rs.close();
+            st.close();
+            cx.close();
+        }catch (ClassNotFoundException | SQLException e) {
+            System.out.println(e);
+        }
+    }
+}
