c749fad1597829ebf9bf91e07b045ea7c91bdc2b
JohnBosc <jnrmengue@gmail.com>
2019-07-12 23:09:19 +0100

    Update database

--- src/TestJDBC.java
+++ src/TestJDBC.java
@@ -5,16 +5,17 @@ public class TestJDBC {
         // Charger la class du driver
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection cx = DriverManager.getConnection("jdbc:mysql://localhost:3309/FlightSchedule?createDatabaseIfNotExist=true && useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "");
-//            Statement st = cx.createStatement();
-//            ResultSet rs = st.executeQuery("SELECT * FROM destination");
-//            while (rs.next()) {
-//                String name = rs.getString("name");
-//                int id = rs.getInt("id");
-//                System.out.println("ID " + id + " is for " + name);
-//            }
-//            rs.close();
-//            st.close();
+            Connection cx = DriverManager.getConnection("jdbc:mysql://localhost:3309/cef506?createDatabaseIfNotExist=true && useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "");
+            Statement st = cx.createStatement();
+            int nb = st.executeUpdate("INSERT INTO destination VALUES (3, 'Libreville')" );
+            ResultSet rs = st.executeQuery("SELECT * FROM destination");
+            while (rs.next()) {
+                String name = rs.getString("name");
+                int id = rs.getInt("id");
+                System.out.println("ID " + id + " is for " + name);
+            }
+            rs.close();
+            st.close();
             cx.close();
         }catch (ClassNotFoundException | SQLException e) {
             System.out.println(e);
